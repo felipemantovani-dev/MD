8f27108b1ab2eed97fef7112abfad411f8c94040
Felipe Santhiago Mantovani <144163629+felipemantovani-dev@users.noreply.github.com>
2024-08-08 20:39:34 +0000

    Pending changes exported from your codespace

---
+++ script.js
@@ -0,0 +1,20 @@
+//slide logic
+
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    nextImg()
+}, 5000) // <--- time
+
+function nextImg() {
+    cont++
+
+    if (cont > 4) {
+        cont = 1
+    } 
+
+    document.getElementById('radio'+cont).checked = true
+}

dc30bbb9983cd69d58a5f81aa7782b8321d31731
Felipe <felipemantovani.dev@gmail.com>
2024-08-20 16:28:03 -0400

    Commit 1.0

--- script.js
+++ script.js
@@ -1,20 +1,113 @@
 //slide logic
 
-var radio = document.querySelector('.manual-btn')
-var cont = 1
+var radio = document.querySelector(".manual-btn");
+var cont = 1;
 
-document.getElementById('radio1').checked = true
+document.getElementById("radio1").checked = true;
 
 setInterval(() => {
-    nextImg()
-}, 5000) // <--- time
+  nextImg();
+}, 5000); // <--- time
 
 function nextImg() {
-    cont++
+  cont++;
 
-    if (cont > 4) {
-        cont = 1
-    } 
+  if (cont > 4) {
+    cont = 1;
+  }
 
-    document.getElementById('radio'+cont).checked = true
+  document.getElementById("radio" + cont).checked = true;
 }
+
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const mess = document.getElementById("message");
+
+function SendEmail() {
+  const bodyMessage = `Full Name : ${fullName.value}<br> Email : ${email.value}<br> Phone : ${phone.value}<br> Subject : ${subject.value}<br> message : ${mess.value}`;
+
+  Email.send({
+    SecureToken: "cec91d87-a77c-497b-9344-1bba663ffec4",
+    To: "dominio.moveis@gmail.com",
+    From: "dominio.moveis@gmail.com",
+    Subject: subject.value,
+    Body: bodyMessage,
+  }).then((message) => {
+    if (message == "OK") {
+      Swal.fire({
+        title: "Sucesso!",
+        text: "Mensagem enviada com sucesso!",
+        icon: "success",
+      });
+    }
+  });
+}
+
+function checkInputs() {
+  const items = document.querySelectorAll(".item");
+
+  for (const item of items) {
+    if (item.value == "") {
+      item.classList.add("error");
+      item.parentElement.classList.add("error");
+    }
+
+    if (items[1].value != "") {
+      checkEmail();
+    }
+
+    items[1].addEventListener("keyup", () => {
+      checkEmail();
+    });
+
+    item.addEventListener("keyup", () => {
+      if (item.value != "") {
+        item.classList.remove("error");
+        item.parentElement.classList.remove("error");
+      } else {
+        item.classList.add("error");
+        item.parentElement.classList.add("error");
+      }
+    });
+  }
+}
+
+function checkEmail() {
+  const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+  const errorTxtEmail = document.querySelector(".error-text.email");
+
+  if (!email.value.match(emailRegex)) {
+    email.classList.add("error");
+    email.parentElement.classList.add("error");
+
+    if (email.value != "") {
+      errorTxtEmail.innerText = "Insira um email valido";
+    } else {
+      errorTxtEmail.innerText = "Esse campo é obrigatório";
+    }
+  } else {
+    email.classList.remove("error");
+    email.parentElement.classList.remove("error");
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInputs();
+
+  if (
+    !fullName.classList.contains("error") &&
+    !email.classList.contains("error") &&
+    !phone.classList.contains("error") &&
+    !subject.classList.contains("error") &&
+    !mess.classList.contains("error")
+  ) {
+    SendEmail();
+
+    form.reset();
+    return false;
+  }
+});
